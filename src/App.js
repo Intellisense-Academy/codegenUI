e62e894e267bb03c8ad0d4203836583127b2edcd
Ganesh <134919900+kganesh09@users.noreply.github.com>
2025-05-14 19:24:34 +0530

    Login and logout Ui created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/JSX/Login'
+import Dashboard from './pages/JSX/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />          {/* Default to login */}
+        <Route path="/dashboard" element={<Dashboard />} /> {/* Dashboard route */}
+      </Routes>
+    </Router>
   );
 }
 
